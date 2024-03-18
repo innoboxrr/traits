b6b208a7d4918b699f020cec9dce49ca7419d0e1
Homero Raul <h.raulvc@gmail.com>
2024-03-17 19:26:15 -0600

    Update AppServiceProvider.php

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace Innoboxrr\Traits\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+
 class AppServiceProvider extends ServiceProvider
 {
 
@@ -17,7 +18,9 @@ class AppServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole())  {
             $this->publishes([__DIR__.'/../../config/innoboxrrtraits.php' => config_path('innoboxrrtraits.php')], 'config');
-            $this->load(__DIR__.'/../Console/Commands');
+            $this->commands([
+                \Innoboxrr\Traits\Console\Commands\RegeneratePayloadCommand::class,
+            ]);
         }
 
     }
